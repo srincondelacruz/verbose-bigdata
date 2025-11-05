e0c6481343229bb5ed5152a1bdc7df65af86a882
srincondelacruz <sergio.rincon@tajamar365.com>
2025-11-05 13:21:45 +0100

    Completada Práctica 04: Paquete 'utilidades_texto' y actualizado índice

---
+++ practica_04-utilidades_texto/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="utilidades_texto",
+    version="0.1",
+    packages=find_packages(),
+    description="Paquete con herramientas para manipular texto",
+    author="srincondelacruz",
+    author_email="tu_email@ejemplo.com",
+    url="http://www.ejemplo.net/utilidades_texto"
+)
---
+++ practica_04-utilidades_texto/texto/formato.py
@@ -0,0 +1,5 @@
+def en_mayusculas(frase):
+    return frase.upper()
+
+def en_minusculas(frase):
+    return frase.lower()
