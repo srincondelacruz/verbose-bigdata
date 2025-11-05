5171483c92061ca45ee8727d3eb728635737630f
Sergio <14425ab@gmail.com>
2025-11-05 11:09:12 +0100

    Actualizada Práctica 01 a v0.2 (función y docs)

--- practica_01-distribucion/paquete/hola/saludos.py
+++ practica_01-distribucion/paquete/hola/saludos.py
@@ -4,3 +4,6 @@ def saludar():
 class Saludo:
     def __init__(self):
         print("👋 Hola, te estoy saludando desde el __init__ de la clase Saludo")
+
+def prueba():
+    print("Esto es una prueba de la nueva version del módulo saludos")
--- practica_01-distribucion/setup.py
+++ practica_01-distribucion/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="paquete",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     description="Este es un paquete de ejemplo",
     author="Mister Nobody",
