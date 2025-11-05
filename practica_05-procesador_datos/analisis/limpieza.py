297e13a4ca15f929d0873ae4a4fba50d6393a713
srincondelacruz <sergio.rincon@tajamar365.com>
2025-11-05 14:04:55 +0100

    Completada Práctica 05: Paquete 'procesador_datos' y actualizado índice

---
+++ practica_05-procesador_datos/analisis/limpieza.py
@@ -0,0 +1,2 @@
+def eliminar_nulos(lista):
+    return [x for x in lista if x is not None]
---
+++ practica_05-procesador_datos/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="procesador_datos",
+    version="0.1",
+    packages=find_packages(),
+    description="Paquete para limpieza y cálculo de estadísticas básicas en listas.",
+    author="srincondelacruz",
+    author_email="srincondelacruz@gmail.com",
+    url="http://www.ejemplo.net/procesador_datos"
+)
